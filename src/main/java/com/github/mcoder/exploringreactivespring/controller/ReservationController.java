97974a5e53b4973caa6944765c8dd13d28a0453f
Mohit Pant <MOHITPAN@amdocs.com>
2020-08-15 16:12:31 +0300

    added code for mvc reactive endpoints

--- src/main/java/com/github/mcoder/exploringreactivespring/controller/ReservationController.java
+++ src/main/java/com/github/mcoder/exploringreactivespring/controller/ReservationController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -15,6 +14,7 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+//    this is MVC style creation of REST API, it is useful if there are other api's exposed in this class and they would like to reuse reservationesrvice in some way
     @GetMapping("/api/reservation/{name}")
     public Mono<Reservation> getReservationByPersonName(@PathVariable("name") String name) {
         return Mono.from( reservationService.
